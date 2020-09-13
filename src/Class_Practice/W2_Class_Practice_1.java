ce4fb6b5e0bc7a71f21ae026abe78f3baccdd49e
jaejuna <anayana9988@gmail.com>
2020-09-13 15:23:03 +0900

    initial commit

---
+++ src/Class_Practice/W2_Class_Practice_1.java
@@ -0,0 +1,12 @@
+package Class_Practice;
+
+import java.util.*;
+
+public class W2_Class_Practice_1 {
+    public static void main(String[] args) {
+        GregorianCalendar Calendar = new GregorianCalendar();
+        System.out.println("The current time is " + Calendar.getTime());
+
+        System.out.println("have a good time");
+    }
+}     
