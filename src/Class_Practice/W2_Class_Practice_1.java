597084d83a314ec0854587eaf6adbc0c05eeda0a
jaejuna <anayana9988@gmail.com>
2020-09-13 15:38:47 +0900

    commit test

--- src/Class_Practice/W2_Class_Practice_1.java
+++ src/Class_Practice/W2_Class_Practice_1.java
@@ -1,12 +1,17 @@
 package Class_Practice;
 
 import java.util.*;
+import java.time.*;
 
 public class W2_Class_Practice_1 {
     public static void main(String[] args) {
-        GregorianCalendar Calendar = new GregorianCalendar();
-        System.out.println("The current time is " + Calendar.getTime());
+//        GregorianCalendar Calendar = new GregorianCalendar();
+//        System.out.println("The current time is " + Calendar.getTime());
+//
+//        System.out.println("have a good time");
 
-        System.out.println("have a good time");
+        System.out.println("The current time is" + LocalDateTime.now());
+        System.out.println("" +
+                "Have a good time");
     }
 }
