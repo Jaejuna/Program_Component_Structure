0fac5aebb8eaa49df745c1bdaad27923b3157367
jaejuna <anayana9988@gmail.com>
2020-09-14 15:31:59 +0900

    Variable added

--- src/Class_Practice/W3_Class_Practice_1.java
+++ src/Class_Practice/W3_Class_Practice_1.java
@@ -3,11 +3,16 @@ package Class_Practice;
 //Counting coins
 public class W3_Class_Practice_1 {
     public static void main(String[] args) {
-        System.out.println("9 quarters");
-        System.out.println("2 dimes");
-        System.out.println("0 nickels");
-        System.out.println("6 pennies");
+        int no_of_quarters = 9;
+        int no_of_dimes = 2;
+        int no_of_nickels = 0;
+        int no_of_pennies = 6;
+
+        System.out.println(no_of_quarters + " quarters");
+        System.out.println(no_of_dimes + "2 dimes");
+        System.out.println(no_of_nickels + "0 nickels");
+        System.out.println(no_of_pennies + "6 pennies");
         System.out.println("The total is: ");
-        System.out.println(9 * 25 + 2 * 10 + 1 * 5 + 6 * 1);
+        System.out.println(no_of_quarters * 25 + no_of_dimes * 10 + no_of_nickels * 5 + no_of_pennies * 1);
     }
 }
