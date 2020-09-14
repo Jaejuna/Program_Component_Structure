c0e7a225e80d33f24131bfae673b11d16bdf8387
jaejuna <anayana9988@gmail.com>
2020-09-14 15:20:30 +0900

    Counting coins

---
+++ src/Class_Practice/W3_Class_Practice_1.java
@@ -0,0 +1,13 @@
+package Class_Practice;
+
+//Counting coins
+public class W3_Class_Practice_1 {
+    public static void main(String[] args) {
+        System.out.println("9 quarters");
+        System.out.println("2 dimes");
+        System.out.println("0 nickels");
+        System.out.println("6 pennies");
+        System.out.println("The total is: ");
+        System.out.println(9 * 25 + 2 * 10 + 1 * 5 + 6 * 1);
+    }
+}
