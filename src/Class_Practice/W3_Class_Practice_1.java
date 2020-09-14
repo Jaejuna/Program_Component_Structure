807d8b4bc841235d3b61d26269ae44ed05eb1543
jaejuna <anayana9988@gmail.com>
2020-09-14 15:49:08 +0900

    Into dallor

--- src/Class_Practice/W3_Class_Practice_1.java
+++ src/Class_Practice/W3_Class_Practice_1.java
@@ -9,9 +9,10 @@ public class W3_Class_Practice_1 {
         int no_of_pennies = 6;
 
         System.out.println(no_of_quarters + " quarters");
-        System.out.println(no_of_dimes + "2 dimes");
-        System.out.println(no_of_nickels + "0 nickels");
-        System.out.println(no_of_pennies + "6 pennies");
-        System.out.println("The total is: " + (no_of_quarters * 25 + no_of_dimes * 10 + no_of_nickels * 5 + no_of_pennies * 1));
+        System.out.println(no_of_dimes + " dimes");
+        System.out.println(no_of_nickels + " nickels");
+        System.out.println(no_of_pennies + " pennies");
+        int total = no_of_quarters * 25 + no_of_dimes * 10 + no_of_nickels * 5 + no_of_pennies * 1;
+        System.out.println("The total is: $" + total / 100 + "." + total % 100);
     }
 }
