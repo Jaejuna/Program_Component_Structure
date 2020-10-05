83b7aa6d65e8ad6fab344baa5defb7ebe4aca8b0
Jaejuna <anayana9988@gmail.com>
2020-10-05 23:17:08 +0900

    Lab4_1 updated

--- src/Lab/Lab_4/MathOperations.java
+++ src/Lab/Lab_4/MathOperations.java
@@ -4,11 +4,13 @@ import java.text.*;
 
 public class MathOperations {
 
+    //변수 i의 역수를 리턴 해주는 메소드
     public double inverse(int i){
         double d = 1.0 / i;
         return d;
     }
 
+    //변수 i의 역수를 표준 실생창에 프린트 해주는 메소드
     public void printInverse(int i){
         DecimalFormat formatter = new DecimalFormat("0.000");
         double d = 1.0 / i;
@@ -16,6 +18,7 @@ public class MathOperations {
         System.out.println(s);
     }
 
+    //변수 i의 역수를 pattern 문자열이 제시한 모맷으로 표준 실행창에 프린트 해주는 메소드
     public void printInverse(int i, String pattern){
         DecimalFormat formatter = new DecimalFormat(pattern);
         double d = 1.0 / i;
@@ -23,6 +26,7 @@ public class MathOperations {
         System.out.println(s);
     }
 
+    //변수 i의 역수를 DecimalFormat 객체가 제시한 포맷으로 표준 실행창에 프린트트
     public void printInverse(int i, DecimalFormat formatter){
         double d = 1.0 / i;
         String s = formatter.format(d);
