14a6c1ae056e52b1a0b4d41e7982ef3f40d3ce10
Jaejuna <anayana9988@gmail.com>
2020-11-09 15:45:10 +0900

    Lab7 Hand class completed

--- src/Lab/Lab_7/cardgame/Hand.java
+++ src/Lab/Lab_7/cardgame/Hand.java
@@ -15,16 +15,29 @@ package Lab.Lab_7.cardgame;
         /** showHand - 들고 있는 카드를 모두 실행창에 프린트한다.
          *             카드가 없으면 카드가 없다고 프린트한다. */
         public void showHand() {
-
-
+            if (number_of_cards ==0){
+                System.out.println("들고있는 카드가 없습니다.");
+            }
+            else{
+                Card card;
+                for (int i = 0; i < number_of_cards; i++){
+                    card = hand[i];
+                    System.out.println("#"+ (i+1) + " = " + card.getSuit() + " " + card.getRank());
+                }
+            }
         }
 
         /** receiveCard - 카드를 한장 받는다. 한도(배열 hand의 크기)를 초과하면 받을 수 없다.
          * @param c - 카드
          * @return 성공적으로 받았으면 true, 그렇지 않으면 false */
         public boolean receiveCard(Card c) {
-
-
+            if (number_of_cards< hand.length){
+                hand[number_of_cards] = c;
+                number_of_cards += 1;
+                return true;
+            }
+            else
+                return false;
         }
 
         /** removeCard - 들고 있는 카드 중에서 하나를 버린다.
@@ -34,9 +47,12 @@ package Lab.Lab_7.cardgame;
             if (number_of_cards > 0) {
                 int picked = (int)(Math.random() * number_of_cards);
                 Card card_to_remove = hand[picked];
-
-
-
+                if (number_of_cards - 1 > picked){
+                    for (int i = picked + 1; i < number_of_cards ;i++ )
+                        hand [i-1] = hand[i];
+                }
+                number_of_cards -= 1;
+                return card_to_remove;
             }
             else
                 return null;
